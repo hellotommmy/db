383e6ea5984552c1550828e0fd0fe136c9db5aa7
hellotommmy <1728422631@qq.com>
2017-01-03 22:39:07 +0800

    Add files via upload

--- comm.h
+++ comm.h
@@ -1,29 +1,21 @@
-#include <stdio.h>
-#include "com.h"
-#include <unistd.h>
-#include <dirent.h>
-#include <string.h>
 #ifndef COMM_INCLUDED
 #define COMM_INCLUDED
 #define BUFFER_LEN 1000
 #define MAX_TABLES 100
-#define MAX_ITEMS_IN_TABLE 100
-#define MAX_VARCHAR_LEN 128
+#define MAX_ITEMS_IN_TABLE PAGE_LEN/MAX_VARCHAR_LEN
+#define MAX_VARCHAR_LEN 256
+#define PAGE_LEN 4096
 #define MAX_TABLE_NAME_LEN 128
 #define NUM_RESERVED_WORDS 13
 #define MAX_WORD_LEN 32
 #define NUM_TYPES 2
 #define MAX_RESERVED_WORD_LEN 8//varchar
-#define BITNUM 4
-#define ONE_PAGE_ITEM 31
-#define TWO_PAGE_ITEM 62
-#define THREE_PAGE_ITEM 94
-
-#define PAGE_LEN 4096
 //#define TRUE 1
 //#define FALSE 0
 #define ERROR -1
 #define OK 1
+
+#define	RIGHT_NOT_EXIST  2
 typedef enum {
 	FALSE = 0,
 	TRUE = 1,
@@ -33,19 +25,5 @@ typedef struct {
 	int i;
 	char varchar[MAX_VARCHAR_LEN+1];
 }int_or_char;
-typedef struct{
-    int col_num;
-    int col_type[BITNUM];
-    int base;
-    int intnum;
-    int freepage;
-    int datapage;
-    short index[MAX_ITEMS_IN_TABLE];
-    char col_name[MAX_ITEMS_IN_TABLE][MAX_VARCHAR_LEN];
-}table_head;
-
-void settypebit(int col, int *col_type, table_head *head);
-void page_init(int pagenum, FILE *fp);
-bool_t comptypebit(int col, int_or_char *inchar,table_head head);
 
 #endif
--- select_basic.c
+++ select_basic.c
@@ -9,4 +9,50 @@ int_or_char constant)
 需要选择的列 操作种类（编号按ppt），
 常量 （注：如果是select * 情况，则num=0，没有where的情况，op=0）
 */
+#include <string.h>
+#include <stdio.h>
+#include "comm.h"
+int format_check(char *s,char middle_buffer[6][1000]);
+int read_between(char *s,char *left,char *right,char *t);
 
+int main(int argc, char const *argv[])
+{
+	char s[200] = "select tab1.id, max(count) from tab1, tab2 where tab1.id = tab2.id and count <= 30 and tab1.id!=0 group by tab1.id;";
+	char middle_buffer[6][1000];
+	format_check(s,middle_buffer);
+	int i;
+	for (i = 0; i < 6; ++i)
+	{
+		printf("%s\n",middle_buffer[i] );
+	}
+	return 0;
+}
+int format_check(char *s,char middle_buffer[6][1000]){
+	int aggre,join,filter1,filter2;
+	int result[6];
+	result[0] = read_between(s,"select","from",middle_buffer[0]);
+	result[1] = read_between(s,"from","where",middle_buffer[1]);
+	result[2] = read_between(s,"where","and",middle_buffer[2]);
+	result[3] = read_between(s,"and","and",middle_buffer[3]);
+	result[4] = read_between(s,"and","group by",middle_buffer[4]);
+	result[5] = read_between(s,"group by",";",middle_buffer[2]);
+
+}
+
+int read_between(char *s,char *left,char *right,char *t){
+	char * left_pos;
+	char * right_pos;
+	left_pos = strstr(s,left)+strlen(left);
+	right_pos = strstr(s,right);
+	if(left_pos&&right_pos){
+		int len = (int )(right_pos - left_pos);
+		memcpy(t,left_pos,len);
+		memset(t+len,'\0',1);
+		return OK;
+	}
+	else if(left_pos){
+		return RIGHT_NOT_EXIST;
+	}
+	else
+		return ERROR;
+}
