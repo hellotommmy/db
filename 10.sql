a993d0be96fa6de597da55832d3442dbf95e02c1
hellotommmy <1728422631@qq.com>
2017-01-07 15:10:39 +0800

    Add files via upload

--- 10.sql
+++ 10.sql
@@ -2,3 +2,15 @@ select weight, count(*)
 from col4tab1, col5tab1
 where weight >= 20 and col4tab1.id = col5tab1.id
 group by weight;
+
+
+
+
+
+
+
+
+
+select * from col4tab1, col5tab1;
+
+select * from col4tab1, col5tab1 where col4tab1.id = col5tab1.id;
